97ab5df28496a7f532cf6caaee3a3afe219ae4b9
Arthur Monteiro <monteiroamelo@gmail.com>
2026-05-30 09:33:23 -0300

    feat: set preferred device orientations for the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:onebit_dice/app.dart';
 import 'package:onebit_dice/core/analytics/firebase_analytics_service.dart';
@@ -14,6 +15,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final crashlytics = FirebaseCrashlytics.instance;
